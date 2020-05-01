b385861bcc74c01b6aeac70f67c7adadb75ed79d
Armin Ghasemi <teamworks2016@gmail.com>
2020-05-01 17:38:19 +0200

    Creates Models and migrations

---
+++ app/Project.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = ['title'];
+
+    public function tasks(){
+        return $this->hasMany(Task::class, 'project_id');
+    }
+}
---
+++ app/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+            'project_id',
+            'name',
+            'priority',
+        ];
+
+    public function project(){
+        return $this->belongsTo(Project::class, 'project_id');
+    }
+}
